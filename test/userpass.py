472a1e46107c22f4e9942d68fcf74565ad0e78ef
Sheikh Ac-Cryptic <iamlonelypoet@gmail.com>
2020-10-01 00:50:23 -0400

    Improved the readability and the syntax of the code

--- test/userpass.py
+++ test/userpass.py
@@ -2,16 +2,16 @@
 import getpass
 import sys
 
-print 'Welcome'
+print 'LOG In'
 
 user = raw_input('Username: ')
 
 # note the stream param, if leave blank, zio won't read password prompt, because it's echoed back
 # from tty stdin, while zio handle stdin and stdout/stderr in two ttys.
-pswd = getpass.getpass('Password: ', stream = sys.stdout)
+password = getpass.getpass('Password: ', stream = sys.stdout)
 
-if user == 'user' and pswd == 'pass':
-    print 'Logged in'
+if user == 'user' and password == 'pass':
+    print 'Logging in'
 else:
-    print 'Invalid', repr(user), repr(pswd)
+    print 'Invalid', repr(user), 'or', repr(password)
 
